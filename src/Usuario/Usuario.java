cc8c467a0bb619c3c5b29dfd0e8d306d4a501c78
Ignacio Echezarreta <83033184+echezaig@users.noreply.github.com>
2021-08-28 17:21:10 -0300

    Update Usuario.java

--- src/Usuario/Usuario.java
+++ src/Usuario/Usuario.java
@@ -1,10 +1,31 @@
 package Usuario;
 
 public class Usuario {
-private double presupuesto;
-private double tiempoDisponible;
-private String tipoAtraccionFavorita;
+	private double presupuesto;
+	private double tiempoDisponible;
+	private String tipoAtraccionFavorita;
 
-  //segundo intento!
-   
+	public Usuario(double presupuesto, double tiempoDisponible, String tipoAtraccionFavorita) {
+
+		this.presupuesto = presupuesto;
+		this.tiempoDisponible = tiempoDisponible;
+		this.tipoAtraccionFavorita = tipoAtraccionFavorita;
+	}
+
+	public Usuario() {
+
+	}
+	
+	public double getPresupuesto () {
+		return this.presupuesto;
+	}
+	
+	public double gettiempoDisponible () {
+		return this.tiempoDisponible;
+	}
+
+	public String getTipoAtraccionFavorita() {
+		return tipoAtraccionFavorita;
+	}
+
 }
