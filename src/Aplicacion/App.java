8a3831fe26f0a06d63711225a05946d01f04e68b
Gabriel Pereyra <gabrielp1689@gmail.com>
2021-08-29 20:31:11 -0300

    Se adicionan los metodos que crean los usuarios y las atracciones

--- src/Aplicacion/App.java
+++ src/Aplicacion/App.java
@@ -1,11 +1,21 @@
 package Aplicacion;
+
+import java.util.List;
 import Atraccion.*;
 import Usuario.*;
 
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//Creacion de la lista de usuarios
+		CreacionUsuario crearUsuarios = new CreacionUsuario();
+		List<Usuario> listadoUsuarios = crearUsuarios.crearLista();
+		//Creacion de la lista de atracciones
+		CreacionAtraccion crearAtracciones = new CreacionAtraccion();
+		List<Atraccion> listadoAtracciones = crearAtracciones.crearLista();
+		
+		
+		
 
 	}
 
