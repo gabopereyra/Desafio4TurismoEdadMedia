3ef80e4bf5e69597d3d2a2031c568bab5490d984
Gabriel Pereyra <gabrielp1689@gmail.com>
2021-08-30 17:52:15 -0300

    Se arreglan metodos del main

--- src/Aplicacion/App.java
+++ src/Aplicacion/App.java
@@ -2,6 +2,8 @@ package Aplicacion;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
+
 import Atraccion.*;
 import Usuario.*;
 
@@ -10,6 +12,7 @@ public class App {
 	public static void main(String[] args) {
 		//Creacion de la lista de usuarios
 		List<Usuario> listadoUsuarios = creacionUsuario();
+		
 		//Creacion de la lista de atracciones
 		List<Atraccion> listadoAtracciones = creacionAtraccion();
 		
@@ -21,10 +24,10 @@ public class App {
 	//Metodo creacion Usuario
 	public static List<Usuario> creacionUsuario() {
 		List<Usuario> listado = new ArrayList<Usuario>();
-		
+			
 		Usuario Eowyn = new Usuario(10d, 8d, "Aventura");
 		Usuario Gandalf = new Usuario(100d, 5d, "Paisaje");
-		Usuario Sam = new Usuario(36d, 8d, "Degustación");
+		Usuario Sam = new Usuario(36d, 8d, "Degustacion");
 		Usuario Galadriel = new Usuario(120d, 2d, "Paisaje");
 
 		listado.add(Eowyn);
@@ -41,10 +44,10 @@ public class App {
 		
 		Atraccion Moria = new Atraccion("Moria", 10.0, 2.0, 6, "Aventura");
 		Atraccion MinasTirith = new Atraccion("Minas Tirith", 5.0, 2.5, 25, "Paisaje");
-		Atraccion LaComarca = new Atraccion("La Comarca", 3.0, 6.5, 150, "Degustación");
+		Atraccion LaComarca = new Atraccion("La Comarca", 3.0, 6.5, 150, "Degustacion");
 		Atraccion Mordor = new Atraccion("Mordor", 25.0, 3.0, 4, "Aventura");
 		Atraccion AbismoDeHelm = new Atraccion("Abismo De Helm", 5.0, 2.0, 15, "Paisaje");
-		Atraccion Lothlorien = new Atraccion("Lothlorien", 35.0, 1.0, 30, "Degustación");
+		Atraccion Lothlorien = new Atraccion("Lothlorien", 35.0, 1.0, 30, "Degustacion");
 		Atraccion Erebor = new Atraccion("Erebor", 12.0, 3.0, 32, "Paisaje");
 		Atraccion BosqueNegro = new Atraccion("BosqueNegro", 3.0, 4.0, 12, "Aventura");
 
