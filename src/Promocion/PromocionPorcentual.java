67aa929b2a9ce92d4693de80f7073b24f98bf9dc
Ricardo Tolomei <87551219+32514243@users.noreply.github.com>
2021-09-02 17:34:39 -0300

    Update PromocionPorcentual.java
    Agrego posibles métodos clase promoción porcentual

--- src/Promocion/PromocionPorcentual.java
+++ src/Promocion/PromocionPorcentual.java
@@ -1,5 +1,17 @@
 package Promocion;
+import Usuario.Usuario;
 
 public class PromocionPorcentual extends Promocion{
-
+	
+	
+	private double costo;
+	private int costoCondescuento;
+	
+	
+	public void descuentoVeintePorciento() {
+		if(Usuario.comprarEnEfectivo()) {this.costo= costo*0.80;
+			
+		}
+	}
 }
+
