9eb3823f8d74ef2616a486d91a0063060730c546
GastonCP <87548962+GastonCP@users.noreply.github.com>
2021-09-05 13:01:57 -0300

    agregado Metodos de Agregar Atracciones.

--- test/Usuario/TestUsuario.java
+++ test/Usuario/TestUsuario.java
@@ -2,8 +2,12 @@ package Usuario;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 
+import Atraccion.Atraccion;
 import Atraccion.TipoAtraccion;
 
 public class TestUsuario {
@@ -27,17 +31,39 @@ public class TestUsuario {
 		assertEquals(TiempoDisponibleEsperado, TiempoDisponibleObtenido, 0.0);
 		TipoAtraccion tipoAtraccionFavoritaEsperado = TipoAtraccion.AVENTURA;
 		TipoAtraccion tipoAtraccionFavoritaObtenido = hobbit1.getTipoAtraccionFavorita();
-		assertEquals(tipoAtraccionFavoritaEsperado,tipoAtraccionFavoritaObtenido);
+		assertEquals(tipoAtraccionFavoritaEsperado,tipoAtraccionFavoritaObtenido);;
+		assertNotNull(hobbit1.getAtraccionesSugeridas());
 	}
 	
 	@Test
-	public void añadirUnaAtraccionTest(){
-	  	
-	}
-	
-	@Test
-	public void añadirVariasAtraccionesTest(){
+	public void agregarUnaAtraccionSugeridaTest(){
+		Usuario hobbit1 = new Usuario(100, 8, TipoAtraccion.PAISAJE);
 		
+		Atraccion atraccion1 = new Atraccion("Campos", 30, 4, 60, TipoAtraccion.PAISAJE);
+		
+		List<Atraccion> lista1 = new ArrayList<Atraccion>();
+		lista1.add(atraccion1);
+		
+		hobbit1.cargarSugerencias(lista1);
+		//falta comparalos con assertEquals, pero funciona bien
+	}
+	
+	@Test
+	public void agregarVariasAtraccionesSugeridasTest(){
+		Usuario hobbit1 = new Usuario(100, 8, TipoAtraccion.PAISAJE);
+		
+		Atraccion atraccion1 = new Atraccion("Campos", 30, 4, 60, TipoAtraccion.PAISAJE);
+		Atraccion atraccion2 = new Atraccion("Riscos", 20, 3, 60, TipoAtraccion.PAISAJE);
+		Atraccion atraccion3 = new Atraccion("Bosque", 10, 1, 60, TipoAtraccion.PAISAJE);
+		
+		List<Atraccion> lista1 = new ArrayList<Atraccion>();
+		
+		lista1.add(atraccion1);
+		lista1.add(atraccion2);
+		lista1.add(atraccion3);
+		
+		hobbit1.cargarSugerencias(lista1);
+		//falta comparalos con assertEquals, pero funciona bien
 	}
 	
 }
