c8e6f8bf0ee38e2aed540c3c68504f2aa9af7852
GastonCP <87548962+GastonCP@users.noreply.github.com>
2021-09-14 02:23:17 -0300

    Añadido Interface para dibujar.

---
+++ src/Aplicacion/Interface.java
@@ -0,0 +1,27 @@
+package Aplicacion;
+
+public class Interface {
+
+	public void menu() {
+		System.out.println("||-------------------------------------------------||");
+		System.out.println("|| MENU                                            ||");
+		System.out.println("||-------------------------------------------------||");
+		System.out.println("||(digite el numero de la opcion correspondiente...||");
+		System.out.println("||(luego presiones la tecla ENTER)                 ||");
+		System.out.println("||                                                 ||");
+		System.out.println("|| 1 -> para mostrar los usuarios cargados.        ||");
+		System.out.println("|| 2 -> para mostrar las atracciones cargadas.     ||");
+		System.out.println("|| 3 -> para mostrar las promociones cargadas      ||");
+		System.out.println("||                                                 ||");
+		System.out.println("|| 9 -> para finalizar el programa.                ||");
+		System.out.println("||-------------------------------------------------||");
+	}
+	
+	public void salir() {
+		System.out.println("||-------------------------------------------------||");
+		System.out.println("|| Programa finalizado... gracias por testear      ||");
+		System.out.println("||-------------------------------------------------||");
+	}
+	
+	
+}
