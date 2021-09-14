8db041b21d5f4a04b788a99f6232d45848167ff0
GastonCP <87548962+GastonCP@users.noreply.github.com>
2021-09-14 12:33:24 -0300

    mostrar Atracciones añadido.

--- src/Aplicacion/Interface.java
+++ src/Aplicacion/Interface.java
@@ -2,10 +2,11 @@ package Aplicacion;
 
 import java.util.List;
 
-import Usuario.*;
+import Usuario.Usuario;
+import Atraccion.Atraccion;
 
 public class Interface {
-	
+
 	public void inicio() {
 		System.out.println("||=================================================||");
 		System.out.println("||(digite el numero de la opcion correspondiente...||");
@@ -14,6 +15,7 @@ public class Interface {
 	}
 
 	public void menu() {
+		System.out.println();
 		System.out.println("||=================================================||");
 		System.out.println("|| MENU                                            ||");
 		System.out.println("||=================================================||");
@@ -26,24 +28,55 @@ public class Interface {
 		System.out.println("||=================================================||");
 	}
 
+	public void noDisponible() {
+		System.out.println();
+		System.out.println("||=================================================||");
+		System.out.println("|| Entrada no valida.                              ||");
+		System.out.println("|| Por favor, ingrese una opcion valida.           ||");
+		System.out.println("||=================================================||");
+	}
+
 	public void salir() {
+		System.out.println();
 		System.out.println("||=================================================||");
 		System.out.println("|| Programa finalizado... gracias por testear      ||");
 		System.out.println("||=================================================||");
 	}
 
-	public void motrarUsuario(Usuario user) {
+	private void motrarUsuario(Usuario usuario) {
 		System.out.println("||=================================================||");
-		System.out.println("|| Nombre de Usuario -> " + user.getNombre());
-		System.out.println("|| preferencias      -> " + user.getTipoAtraccionFavorita());
-		System.out.println("|| Dinero en Cuenta  ->                       $" + user.getPresupuesto() + " ||");
-		System.out.println("|| Horas disponibles ->                  " + user.getTiempoDisponible() + " Horas ||");
+		System.out.println("|| Nombre de Usuario -> " + usuario.getNombre());
+		System.out.println("|| preferencias      -> " + usuario.getTipoAtraccionFavorita());
+		System.out.println("|| Dinero en Cuenta  ->                      $" + usuario.getPresupuesto() + " ||");
+		System.out.println("|| Horas disponibles ->                  " + usuario.getTiempoDisponible() + " Horas ||");
 
 	}
-	
-	public void mostrarTodosLosUsuarios(List<Usuario> usuarios) {
+
+	public void mostrarLosUsuarios(List<Usuario> usuarios) {
+		System.out.println();
+		System.out.println("||=================================================||");
+		System.out.println("|| Mostrando todos los Usuarios cargados...        ||");
 		for (Usuario i : usuarios) {
 			motrarUsuario(i);
+		}
+		System.out.println("||=================================================||");
+		System.out.println();
+	}
+
+	private void mostrarAtraccion(Atraccion a) {
+		System.out.println("||=================================================||");
+		System.out.println("|| Nombre ->  " + a.getNombre());
+		System.out.println("|| Tipo   ->  " + a.getTipoAtraccion());
+		System.out.println("|| Costo  ->                                  $" + a.getCostoAtraccion() + " ||");
+		System.out.println("|| Duracion de la Atraccion ->           " + a.getTiempoNecesario() + " Horas ||");
+	}
+
+	public void mostrarLasAtracciones(List<Atraccion> atracciones) {
+		System.out.println();
+		System.out.println("||=================================================||");
+		System.out.println("|| Mostrando todas las Atracciones cargadas...     ||");
+		for (Atraccion i : atracciones) {
+			mostrarAtraccion(i);
 		}
 		System.out.println("||=================================================||");
 		System.out.println();
